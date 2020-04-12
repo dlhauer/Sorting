19e36f1ee3966857b874e15aea96a17f84d696fa
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-12 11:32:16 -0700

    completes iterative binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -16,7 +16,14 @@ def binary_search(arr, target):
     low = 0
     high = len(arr)-1
 
-    # TO-DO: add missing code
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid-1
+        else:
+            low = mid+1
 
     return -1  # not found
 
