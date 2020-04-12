87e7161d01c6f7eec12140100a7e01b557523ad1
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-12 11:32:27 -0700

    adds test cases

--- src/searching/test_searching.py
+++ src/searching/test_searching.py
@@ -18,6 +18,8 @@ class SearchingTests(unittest.TestCase):
 
         self.assertEqual(binary_search(arr1, -8), 1)
         self.assertEqual(binary_search(arr1, 0), 6)
+        self.assertEqual(binary_search(arr1, -9), 0)
+        self.assertEqual(binary_search(arr1, 9), 13)
         self.assertEqual(binary_search(arr2, 6), -1)
         self.assertEqual(binary_search(arr2, 0), -1)
 
